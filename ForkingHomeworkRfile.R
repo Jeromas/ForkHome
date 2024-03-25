7cd56f3c15d53dfd5507726af0d1cf0be42765c1
Hatice Erdogan <94992749+haticerdogan@users.noreply.github.com>
2024-03-25 19:29:40 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,15 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+library(data.table)
+
+# Convert diamonds dataset to data.table
+diamonds <- as.data.table(diamonds)
+
+# Select columns carat, cut, and price
+diamonds_subset <- diamonds[, .(carat, cut, price)]
+diamonds_subset <- diamonds_subset[cut == "Premium" & price > 7000 & price < 10000]
+diamonds_subset <- diamonds_subset[order(-carat)]
+result <- diamonds_subset[1:20]
+print(result)
